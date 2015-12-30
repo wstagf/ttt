414aa842a508fd9d059a08a3de07abc0761fa9e5
wstagf <wstagf@gmail.com>
2015-12-30 14:07:41 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
         <title>SACA</title>
         <meta charset="utf-8">
         
-        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css" >
+		<link rel="stylesheet" href="css/angular-toastr.min.css" type="text/css" />
         
     </head>
     <body>
@@ -47,18 +48,40 @@
                         
                     </div>
                     <div class="col-xs-12 col-sm-5">
-                        <h4>Instruções de login</h4>
-                        <p>Cuide letras maiusculas e minusculas</p>
+                        <label>Formulario de Registro de Usuario</label>
+						<form class="form-horizontal" ng-submit="inserirUsuario()">
+                          <div class="form-group">
+                            <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
+                            <div class="col-sm-10">
+                              <input type="text" class="form-control" id="inputEmail3" placeholder="Usuário" ng-model="loginAdd.usuario" required>
+                            </div>
+                          </div>
+                          <div class="form-group">
+                            <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
+                            <div class="col-sm-10">
+                              <input type="password" class="form-control" id="inputPassword3" placeholder="Senha" ng-model="loginAdd.senha" required>
+                            </div>
+                          </div>
+                          
+                          <div class="form-group">
+                            <div class="col-sm-offset-2 col-sm-10">
+                              <button type="submit" class="btn btn-default">Efetuar Login</button>
+                            </div>
+                          </div>
+                        </form>
                     </div>
                 </div>
                 
             </div>
             
-            
         </div>
-        
+
+				 
+
         <script src="js/angular/angular.min.js"></script>
+		<script src="js/angular/angular-toastr.tpls.min.js"></script>
         <script src="js/app.module.js"></script>
         <script src="js/loginController.js"></script>
+
     </body>
 </html>
