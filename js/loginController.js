c2970258c7c2eec1d713723fcf5f108ac34e6587
wstagf <wstagf@gmail.com>
2015-12-30 13:53:17 -0300

    Update loginController.js

--- js/loginController.js
+++ js/loginController.js
@@ -1,5 +1,10 @@
-app.controller('loginController', function ($scope, $http) {
+app.controller('loginController', function ($scope, $http, toastr) {
     $scope.login = {
+        usuario: '',
+        senha: ''
+    };
+
+    $scope.loginAdd = {
         usuario: '',
         senha: ''
     };
@@ -11,8 +16,34 @@ app.controller('loginController', function ($scope, $http) {
                 if (data.logado) {
                     window.location = "home.php"
                 } else {
-                    alert('Verifique usuario ou senha')
+                    toastr.info('Verifique o usuário e a senha', 'Atenção');
                 }
             });
     }
+
+    $scope.inserirUsuario = function () {
+        $http.post('api/registration', $scope.loginAdd)
+            .success(function (data) {
+                console.log(data);
+                if (!data.erro) {
+                    $scope.loginAdd = {
+                        usuario: '',
+                        senha: ''
+                    };
+                    toastr.success('Usuário adicionado!', 'Sucesso');
+                } else {
+                    alert('Deu erro')
+                }
+            })
+            .error(function () {
+                toastr.error('Erro no servidor', 'Erro');
+            });
+    }
+
+    $scope.sair = function () {
+        toastr.info('Saindo do sistema', 'Atenção');
+        $http.get('api/logout')
+        window.location = "index.php"
+    }
+
 })
