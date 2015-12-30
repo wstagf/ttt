25dc2a2bf5cf85f0117d5748bfe99d63dd544255
wstagf <wstagf@gmail.com>
2015-12-30 09:52:39 -0300

    Update bd.sql

--- bd.sql
+++ bd.sql
@@ -2,8 +2,9 @@ CREATE TABLE `usuario` (
 	`id` INT(10) NOT NULL AUTO_INCREMENT,
 	`usuario` VARCHAR(50) NOT NULL,
 	`senha` VARCHAR(50) NOT NULL,
+	`pass` VARCHAR(50) NOT NULL,
 	PRIMARY KEY (`id`),
 	UNIQUE INDEX `usuario` (`usuario`)
 )
 
-INSERT INTO `usuario` (`usuario`, `senha`) VALUES ('thiago', '123');
+INSERT INTO `usuario` (`usuario`, `senha`, `pass`) VALUES ('thiago', '123', '123' );
