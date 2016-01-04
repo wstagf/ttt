497ef7c81dfe52301384d65aaf2909e27cb9e2a1
wstagf <wstagf@gmail.com>
2016-01-04 10:57:13 -0300

    Create dbteste.php

---
+++ dbteste.php
@@ -0,0 +1,42 @@
+<?php
+
+session_start();
+    if(!isset($_SESSION['logado'])){
+        header("Location: index.php");
+}
+
+
+  
+	//try {
+      //  $dbh = new PDO('mysql:host=localhost;port=3306;dbname=angularnoticiasv1', 'root', '', array( PDO::ATTR_PERSISTENT => false));
+	//	$usuario = 'usua';
+	///	$pass   = '111';
+	//	$senha = md5($pass); 
+	//	$sql = "INSERT INTO usuario (usuario, senha, pass) VALUES ('".$usuario."', '".$senha."', '".$pass."');";
+//
+ //       $stmt = $dbh->prepare("INSERT INTO usuario (usuario, senha, pass) VALUES ('".$usuario."', '".$senha."', '".$pass."');");
+ ///
+   //    // call the stored procedure
+    //    $stmt->execute();
+
+     
+    
+   // } catch (PDOException $e) {
+    //    print "Error!: " . $e->getMessage() . "<br/>";
+     //   die();
+   // }
+?>
+
+
+
+?>
+
+<html ng-app="app">
+	<head>
+
+	</head>
+	<home>
+	 "çãõéíáú"
+	</home>
+</html>
+  
