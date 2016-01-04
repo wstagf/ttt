cbf0abd3247039491831df89cbe1a065ba7701de
wstagf <wstagf@gmail.com>
2016-01-04 13:47:23 -0300

    Update paginaInicialController.js

--- js/paginaInicialController.js
+++ js/paginaInicialController.js
@@ -10,6 +10,7 @@ app.controller('paginaInicialController', function ($scope, $http, toastr) {
 
     $scope.navegar = function (parametro) {
         $scope.pagina = parametro + '.php';
+        $scope.listarUsuarios();
         
     }
 
@@ -21,5 +22,29 @@ app.controller('paginaInicialController', function ($scope, $http, toastr) {
     }
     $scope.showListUsuario = function () {
         $scope.addUsuario = false;
+        $scope.listarUsuarios();
     }
+
+    $scope.listaUsuarios = {};
+    $scope.listarUsuarios = function () {
+        $http.get('api/listarUsuarios')
+            .success(function (data) {
+                //$scope.listaUsuarios = data.$usuarios;
+                console.log(data);
+                if (data.result.length == 0) {
+                    toastr.info('Não foram encontrados registos', 'Informação');
+                } else {
+                    $scope.listaUsuarios = data.result;
+                    console.log($scope.listaUsuarios);
+                }
+            })
+            .error(function (data) {
+                //alert("Falha em obter usuarios");
+                //console.log(data);
+                toastr.error('Erro ao localizar Usuarios', 'Erro');
+            });
+    };
+
+
+
 })
