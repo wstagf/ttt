48a7c7214359a481cbbd5035ac9d75cd166517ad
wstagf <wstagf@gmail.com>
2016-01-04 15:30:01 -0300

    Update usuarios.php

--- usuarios.php
+++ usuarios.php
@@ -78,7 +78,7 @@
 				<div class="panel-body" ng-show="updtUsuario">
                     <div class="dataTable_wrapper">
                         <label>Formulario de Edição de Usuario</label>
-						<form class="form-horizontal" ng-submit="atualizarUsuario()">
+						<form class="form-horizontal" ng-submit="alterarUsuario()">
 							<div class="form-group">
 							<label for="inputEmail3" class="col-sm-2 control-label">ID</label>
 							<div class="col-sm-10">
