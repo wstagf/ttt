fda8dcaaa3e1b574bd8f37eaf9936f039182a05e
wstagf <wstagf@gmail.com>
2016-01-04 10:53:04 -0300

    Update paginaInicialController.js

--- js/paginaInicialController.js
+++ js/paginaInicialController.js
@@ -1,9 +1,25 @@
 app.controller('paginaInicialController', function ($scope, $http, toastr) {
-  
+    
     $scope.sair = function () {
         toastr.info('Saindo do sistema', 'Atenção');
         $http.get('api/logout')
         window.location = "index.php"
     }
+    
+    $scope.pagina = 'homeContent.php';
 
+    $scope.navegar = function (parametro) {
+        $scope.pagina = parametro + '.php';
+        
+    }
+
+
+    
+    $scope.addUsuario = false;
+    $scope.showAddUsuario = function () {
+        $scope.addUsuario = true;
+    }
+    $scope.showListUsuario = function () {
+        $scope.addUsuario = false;
+    }
 })
