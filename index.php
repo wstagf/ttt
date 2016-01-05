5a849af5756e0babdfe6a3ee9d521b07e6d60775
wstagf <wstagf@gmail.com>
2016-01-05 08:40:03 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,67 +1,63 @@
 <html ng-app="app">
     <head>
-        <title>Lista de Notícias</title>
+		<title>SACA</title>
         <meta charset="utf-8">
-        
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-        
+        <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css" >
+		<link rel="stylesheet" href="css/angular-toastr.min.css" type="text/css" />
     </head>
     <body>
         
-        <div ng-controller="inicialController">       
+        <div ng-controller="loginController">       
             
             <div class="container">
                 <div class="row">
                     <div class="col-xs-12">
                     
                         <div class="page-header">
-                            <h2>Nossas Notícias - Devmedia</h2>
+                            <h3>Efetue Login</h3>
                         </div>
-
-                        <div class="jumbotron">
-                          <h1>Seja bem vindo!</h1>
-                          <p>Sistema de notícias Devmedia V1 usando AngularJS</p>
-                        </div>
-                    
-                    </div>
-                </div>
-            </div>
-            
-            
-            <div class="container">
-                <div class="row">
-                    <div class="col-xs-12">
-                    
-                
                         
-                <div 
-                     class="alert alert-info" 
-                     ng-repeat="item in noticias"
-                >
-                    <h3>
-                        <span class="label label-primary" style="margin-right:10px;">
-                            {{item.noticia.dados.datanoticia}}
-                        </span>  
-                        <a href="verNoticia.php?id={{item.noticia.dados.idnoticia}}">
-                            {{item.noticia.dados.noticiatitulo}}
-                        </a>
-                        </h3>
-                    <p>{{item.noticia.dados.noticiadescricao}}</p>
-                    
-                </div>
-                    
-                    
-                    
-                    
                     </div>
                 </div>
+                
+                <div class="row">
+                    <div class="col-xs-12 col-sm-7">
+                        
+                        <form class="form-horizontal" ng-submit="fazerLogin()">
+                          <div class="form-group">
+                            <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
+                            <div class="col-sm-10">
+                              <input type="text" class="form-control" id="inputEmail3" placeholder="Usuário" ng-model="login.usuario" required>
+                            </div>
+                          </div>
+                          <div class="form-group">
+                            <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
+                            <div class="col-sm-10">
+                              <input type="password" class="form-control" id="inputPassword3" placeholder="Senha" ng-model="login.senha" required>
+                            </div>
+                          </div>
+                          
+                          <div class="form-group">
+                            <div class="col-sm-offset-2 col-sm-10">
+                              <button type="submit" class="btn btn-default">Efetuar Login</button>
+                            </div>
+                          </div>
+                        </form>
+                        
+                    </div>
+                    <div class="col-xs-12 col-sm-5">
+                        
+                    </div>
+                </div>
+                
             </div>
-            
             
         </div>
         
+		
         <script src="js/angular/angular.min.js"></script>
+		<script src="js/angular/angular-toastr.tpls.min.js"></script>
         <script src="js/app.module.js"></script>
-        <script src="js/inicialController.js"></script>
+        <script src="js/loginController.js"></script>
     </body>
 </html>
