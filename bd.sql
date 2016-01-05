35e0e157539c6fa1b0d7d66865e883d615bd317f
wstagf <wstagf@gmail.com>
2016-01-05 14:13:20 -0300

    Update bd.sql

--- bd.sql
+++ bd.sql
@@ -16,6 +16,21 @@ CREATE DATABASE IF NOT EXISTS `angularnoticiasv1` /*!40100 DEFAULT CHARACTER SET
 USE `angularnoticiasv1`;
 
 
+-- Copiando estrutura para tabela angularnoticiasv1.filial
+DROP TABLE IF EXISTS `filial`;
+CREATE TABLE IF NOT EXISTS `filial` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `descricao` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+-- Copiando dados para a tabela angularnoticiasv1.filial: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `filial` DISABLE KEYS */;
+INSERT INTO `filial` (`id`, `descricao`) VALUES
+	(1, 'NOME FILIALA 01 EDITADO');
+/*!40000 ALTER TABLE `filial` ENABLE KEYS */;
+
+
 -- Copiando estrutura para tabela angularnoticiasv1.imagem
 DROP TABLE IF EXISTS `imagem`;
 CREATE TABLE IF NOT EXISTS `imagem` (
@@ -71,13 +86,90 @@ CREATE TABLE IF NOT EXISTS `perfilusuario` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `descricao` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 
 -- Copiando dados para a tabela angularnoticiasv1.perfilusuario: ~1 rows (aproximadamente)
 /*!40000 ALTER TABLE `perfilusuario` DISABLE KEYS */;
 INSERT INTO `perfilusuario` (`id`, `descricao`) VALUES
 	(1, 'Acesso Completo');
 /*!40000 ALTER TABLE `perfilusuario` ENABLE KEYS */;
+
+
+-- Copiando estrutura para tabela angularnoticiasv1.supervisor
+DROP TABLE IF EXISTS `supervisor`;
+CREATE TABLE IF NOT EXISTS `supervisor` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `descricao` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+-- Copiando dados para a tabela angularnoticiasv1.supervisor: ~1 rows (aproximadamente)
+/*!40000 ALTER TABLE `supervisor` DISABLE KEYS */;
+INSERT INTO `supervisor` (`id`, `descricao`) VALUES
+	(1, 'supervisor 1 editado');
+/*!40000 ALTER TABLE `supervisor` ENABLE KEYS */;
+
+
+-- Copiando estrutura para tabela angularnoticiasv1.tipoagencia
+DROP TABLE IF EXISTS `tipoagencia`;
+CREATE TABLE IF NOT EXISTS `tipoagencia` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `descricao` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+-- Copiando dados para a tabela angularnoticiasv1.tipoagencia: ~2 rows (aproximadamente)
+/*!40000 ALTER TABLE `tipoagencia` DISABLE KEYS */;
+INSERT INTO `tipoagencia` (`id`, `descricao`) VALUES
+	(2, 'tipo agencia'),
+	(3, 'tipo agencia 3');
+/*!40000 ALTER TABLE `tipoagencia` ENABLE KEYS */;
+
+
+-- Copiando estrutura para tabela angularnoticiasv1.tipodocumento
+DROP TABLE IF EXISTS `tipodocumento`;
+CREATE TABLE IF NOT EXISTS `tipodocumento` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `descricao` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+-- Copiando dados para a tabela angularnoticiasv1.tipodocumento: ~1 rows (aproximadamente)
+/*!40000 ALTER TABLE `tipodocumento` DISABLE KEYS */;
+INSERT INTO `tipodocumento` (`id`, `descricao`) VALUES
+	(2, 'tipo documento teste');
+/*!40000 ALTER TABLE `tipodocumento` ENABLE KEYS */;
+
+
+-- Copiando estrutura para tabela angularnoticiasv1.tiposituacaocontrato
+DROP TABLE IF EXISTS `tiposituacaocontrato`;
+CREATE TABLE IF NOT EXISTS `tiposituacaocontrato` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `descricao` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+-- Copiando dados para a tabela angularnoticiasv1.tiposituacaocontrato: ~2 rows (aproximadamente)
+/*!40000 ALTER TABLE `tiposituacaocontrato` DISABLE KEYS */;
+INSERT INTO `tiposituacaocontrato` (`id`, `descricao`) VALUES
+	(1, 'situacao de contrato'),
+	(2, 'tipo situacao');
+/*!40000 ALTER TABLE `tiposituacaocontrato` ENABLE KEYS */;
+
+
+-- Copiando estrutura para tabela angularnoticiasv1.tipovenda
+DROP TABLE IF EXISTS `tipovenda`;
+CREATE TABLE IF NOT EXISTS `tipovenda` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `descricao` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+-- Copiando dados para a tabela angularnoticiasv1.tipovenda: ~1 rows (aproximadamente)
+/*!40000 ALTER TABLE `tipovenda` DISABLE KEYS */;
+INSERT INTO `tipovenda` (`id`, `descricao`) VALUES
+	(1, 'tipo venda 01 editado');
+/*!40000 ALTER TABLE `tipovenda` ENABLE KEYS */;
 
 
 -- Copiando estrutura para tabela angularnoticiasv1.usuario
@@ -92,15 +184,14 @@ CREATE TABLE IF NOT EXISTS `usuario` (
   UNIQUE KEY `usuario` (`usuario`),
   KEY `FK_Usuario_PerfilUsuario` (`idPerfilUsuario`),
   CONSTRAINT `FK_Usuario_PerfilUsuario` FOREIGN KEY (`idPerfilUsuario`) REFERENCES `perfilusuario` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=60 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=89 DEFAULT CHARSET=utf8;
 
--- Copiando dados para a tabela angularnoticiasv1.usuario: ~1 rows (aproximadamente)
+-- Copiando dados para a tabela angularnoticiasv1.usuario: ~2 rows (aproximadamente)
 /*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
 INSERT INTO `usuario` (`id`, `usuario`, `senha`, `idPerfilUsuario`, `status`) VALUES
 	(59, '1', 'c4ca4238a0b923820dcc509a6f75849b', 1, 1),
-	(60, '2', 'c81e728d9d4c2f636f067f89cc14862c', 1, 0);
+	(88, 'teste', 'e8d95a51f3af4a3b134bf6bb680a213a', 1, 1);
 /*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-
